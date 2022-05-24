01c7a7ddc99b069723ef7fa8a21041e090cbcb7b
Alina <daxelka@yandex.ru>
2022-05-24 13:28:22 +0100

    first commit

---
+++ deffuant_simple.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+class DeffuantModel:
+    def __init__(self, N_nodes, confidence, cautiousness):
+        self.N_nodes = N_nodes
+        self.node_ids = range(self.N_nodes)
+        self.opinions = []
+        # Deffuant parameters
+        self.confidence = confidence  # restricted to interval (0, 0.5]
+        self.cautiousness = cautiousness  # convergence parameter, mu,  restricted to interval (0, 0.5]
+        self.PRECISION = 0.01  # difference between opinions that are considered identical
+        # convergence parameters
+        self.MAXIMUM_STEPS = int(1e08)
+
+    def interaction(self):
+        # choose two nodes for interaction at random
+        node1, node2 = random.sample(self.node_ids, 2)
+        # get nodes' opinions
+        value1 = self.opinions[node1]
+        value2 = self.opinions[node2]
+        # calculate the distance in nodes' opinion
+        diff = abs(value1 - value2)
+        # calculate opinion update
+        if self.confidence > diff > self.PRECISION:
+            self.opinions[node1] = value1 + self.cautiousness * (value2 - value1)
+            self.opinions[node2] = value2 + self.cautiousness * (value1 - value2)
+
+    def run(self, n_steps):
+        for step in range(n_steps):
+            self.interaction()
+
+
+    def set_opinion(self, opinion_array):
+        self.opinions = list(opinion_array)
+
+    def get_opinion(self):
+        return self.opinions
+
+    def show_opinion_distribution(self, opinions):
+        bins = [0.01 * n for n in range(100)]
+        plt.hist(opinions, bins=bins, density=True)
+        plt.title("Histogram of opinions")
+        plt.show()
+
+
---
+++ single_run_simple_model.py
@@ -0,0 +1,25 @@
+from deffuant_simple import DeffuantModel
+import distribution_tools as tools
+import numpy as np
+
+# Set up model parameters
+N_nodes: int = 100
+epsilon = 0.2
+mu = 0.2
+n_steps = int(1e04)
+
+# Initiate the model
+model = DeffuantModel(N_nodes, epsilon, mu)
+
+# generate distribution for initial conditions
+initial_opinion = tools.uniform_opinion(N_nodes)
+
+# Set initial condition
+model.set_opinion(initial_opinion)
+
+# Run the model for n_steps
+model.run(n_steps)
+final_opinion = model.get_opinion()
+
+# Show opinion distribution
+model.show_opinion_distribution(final_opinion)
